e21a1244dcf4e7c2f56d56a4c6b03e1e41e2d20c
F4uzan <f4uzan@this.is.f4>
2017-01-22 20:26:15 +0700

    install: Check for symlink

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo ---------------
 echo
 echo Make sure this file and the whole repository of Minibuild is cloned / placed in "minibuild" folder in the root of your ROM source.
 echo
-if [ -e ../build.sh ]; then
+if [ -L ../build.sh ]; then
 	echo build.sh already exist in ROM source, stopping installation.
 	echo Please delete or rename existing build.sh in your ROM source.
 	echo
